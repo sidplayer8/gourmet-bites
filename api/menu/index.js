1ee6751b345b8bc7ec04dc888f75d9e6316efc9e
sidplayer8 <sidplayer8@users.noreply.github.com>
2025-12-05 16:57:40 +0800

    Debug: Add version header to menu API

--- api/menu/index.js
+++ api/menu/index.js
@@ -4,6 +4,7 @@ module.exports = async function handler(req, res) {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('x-version', 'debug-timestamp-' + Date.now());
 
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method not allowed' });
